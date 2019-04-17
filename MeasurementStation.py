3b47bffc85a0daa0e4791d2d1737ce15d70eabd8
seplue <32180184+seplue@users.noreply.github.com>
2019-04-17 11:10:36 +0200

    moved test part into test.py

--- MeasurementStation.py
+++ MeasurementStation.py
@@ -4,7 +4,7 @@ import busio
 import adafruit_bme280
 import psycopg2
 # import RPi.GPIO as GPIO
-# todo send more information with sendMeasurements(): bedName, gardenName
+
 # todo change humidity to airhumidity and add earthhumidity
 
 
@@ -47,12 +47,11 @@ class Garden(object):
 
     @staticmethod
     def sendMeasurements(measurements):
-        if True:
-            for i in range(0, len(measurements)):
-                print(str(measurements[i].measurementTime) + ": " +
-                      measurements[i].measurementType + ": " +
-                      str(measurements[i].measurementValue))
-            print("---")
+        for i in range(0, len(measurements)):
+            print(str(measurements[i].measurementTime) + ": " +
+                  measurements[i].measurementType + ": " +
+                  str(measurements[i].measurementValue))
+        print("---")
         # todo change values in .connect() to variables from function init
         conn = psycopg2.connect('host=192.168.1.31 user=pi password=PzFhr2017 dbname=plantguardian_test')
         cursor = conn.cursor()
@@ -123,21 +122,3 @@ class BME_280_Sensor(Sensor):
         else:
             print("add exception here")
             # todo: add throw exception
-
-
-# test
-
-myGarden = Garden("addr", "pi", "pw", "name", 60)
-myBed = Bed("firstBed")
-myBed.addSensor(BME_280_Sensor(["temperature", "humidity", "pressure", "dewPoint"], ""))
-myGarden.addBed(myBed)
-
-myMeasurements = myGarden.collectMeasurements()
-"""
-for o in range(0, len(myMeasurements)):
-    print(str(myMeasurements[o].measurementTime) + ": " +
-          myMeasurements[o].measurementType + ": " +
-          str(myMeasurements[o].measurementValue))
-
-print("---")"""
-myGarden.continuousMeasurement(60)
---
+++ test.py
@@ -0,0 +1,14 @@
+from MeasurementStation import *
+
+# test
+"""Test """
+
+myGarden = Garden("addr", "pi", "pw", "name", 60)
+myBed = Bed("firstBed")
+myBed.addSensor(BME_280_Sensor(["temperature", "humidity", "pressure", "dewPoint"], ""))
+myGarden.addBed(myBed)
+
+myMeasurements = myGarden.collectMeasurements()
+myGarden.sendMeasurements(myMeasurements)
+
+#myGarden.continuousMeasurement(60)
